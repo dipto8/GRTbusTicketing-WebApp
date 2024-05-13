273155bd68b38e1a89775e9e92327466523d2cc0
FAHAD KABIR <hmdipto7@gmail.com>
2024-05-12 22:45:37 -0230

    js updated

--- scripts.js
+++ scripts.js
@@ -1,37 +1,42 @@
+let selectedSeats = [];
 
-// Function to handle click event
 function handleClick(event) {
     // Get the ID of the clicked element
     const id = event.target.id;
-     addbackgroundColor(id)
-     
+  if (selectedSeats.length >= 4) {
+    return;
+  }
 
-     // Total seat 
-     const totalSeatElement = document.getElementById('total-seat')
-     const totalSeatElementText = totalSeatElement.innerText; 
-     const currentSeat = parseInt(totalSeatElementText);
-     const updateSeat = currentSeat - 1 ;
-     totalSeatElement.innerText = updateSeat
-     // selected seat
-     const selectedseatElement = document.getElementById('selected-seats')
-      const selectedseatElementText = selectedseatElement.innerText
-      const currentSelected = parseInt(selectedseatElementText);
-      const totalSeatSelected = currentSelected + 1
-       selectedseatElement.innerText = totalSeatSelected
-       
-     // Total-price
-     const totalpriceElement = document.getElementById('Total-price')
-     const totalpriceElementText = totalpriceElement.innerText
-     const currentTotalPrice = parseInt(totalpriceElementText);
-     const updatedPrice =  currentTotalPrice + 12
-     totalpriceElement.innerText = updatedPrice
-    
+  if (!selectedSeats.includes(id)) {
+    selectedSeats.push(id);
+    addbackgroundColor(id);
+  } else {
+    return;
+  }
+
+  // Total seat
+  const totalSeatElement = document.getElementById("total-seat");
+  const totalSeatElementText = totalSeatElement.innerText;
+  const currentSeat = parseInt(totalSeatElementText);
+  const updateSeat = currentSeat - 1;
+  totalSeatElement.innerText = updateSeat;
+  // selected seat
+  const selectedseatElement = document.getElementById("selected-seats");
+  const selectedseatElementText = selectedseatElement.innerText;
+  const currentSelected = parseInt(selectedseatElementText);
+  const totalSeatSelected = currentSelected + 1;
+  selectedseatElement.innerText = totalSeatSelected;
+
+  // Total-price
+  const totalpriceElement = document.getElementById("Total-price");
+  const totalpriceElementText = totalpriceElement.innerText;
+  const currentTotalPrice = parseInt(totalpriceElementText);
+  const updatedPrice = currentTotalPrice + 12;
+  totalpriceElement.innerText = updatedPrice;
 }
-  
-  // Get all elements with class 'kbd'
-  const kbdElements = document.querySelectorAll('.kbd');
-   for(const kbdElement of kbdElements ){
-    kbdElement.addEventListener('click', handleClick)
-   }
 
-  
+// Get all elements with class 'kbd'
+const kbdElements = document.querySelectorAll(".kbd");
+for (const kbdElement of kbdElements) {
+  kbdElement.addEventListener("click", handleClick);
+}
